e01e41c9f5a2248543a6203fcee3597926cfb8c2
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-06-05 21:59:44 -0300

    real-time-socket.io

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Neste projeto estou desenvolvendo uma aplicação basica de React, Socket.io e Node.js
+
+# Dependencias necessarias
+
+De dentro do diretório **frontend** do aplicativo, execute o seguinte camando:
+
+>**npm i**
+
+De dentro do diretório **server** do aplicativo, execute o seguinte camando:
+
+>**npm i**
+
+Após instalar as dependencias basta executar o seguinte comando em cada diretorio.
+
+>**npm start**
---
+++ frontend/src/chat.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
+
+const myId = uuidv4()
+const socket = io('http://localhost:8080', { transports: ['websocket'] });
+
+socket.on('connect', () => console.log('[IO] connect => A new connection has been established'))
+
+const Chat = () => {
+
+    const [message, updateMessage] = useState('')
+    const [messages, updateMessages] = useState([])
+
+    useEffect(() => {
+        const handleNewMessage = newMessage => updateMessages([...messages, newMessage])
+        socket.on('chat.message', handleNewMessage)
+        return () => socket.off('chat.message', handleNewMessage)
+    }, [messages])
+
+    const handleFormSubmit = event => {
+        event.preventDefault()
+        if (message.trim()) {
+            socket.emit('chat.message', {
+                id: myId,
+                message
+            })
+            updateMessage('')
+        }
+    }
+
+    const handleInputChange = event => updateMessage(event.target.value)
+
+    return (
+        <main className="container" >
+            <ul className="list">
+                {messages.map((m, index) => (
+                    <li className={`list__item list__item--${m.id ===myId ? 'mine' : 'other'}`} key={index}>
+                        <span className={`message message--${m.id === myId ? 'mine' : 'other'}`}>
+                            {m.message}
+                        </span>
+                    </li>
+                ))}
+            </ul>
+            <form className="form" onSubmit={handleFormSubmit}>
+                <input
+                    className="form__field"
+                    onChange={handleInputChange}
+                    placeholder="Type a new message here"
+                    type="text"
+                    value={message}
+                />
+            </form>
+        </main>
+    )
+}
+
+export default Chat
